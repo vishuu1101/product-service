669beea22a341c0195a01637238da7e075a22b3d
vishwanath.batta <viswanath.batta@hyke.ai>
2025-01-08 18:42:37 +0400

    removed print statement

--- util/text_util.py
+++ util/text_util.py
@@ -6,10 +6,8 @@ model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def encode_text(text: str):
     try:
-        print("Encoding text...")
-        embedding = model.encode(text)
-        print("Encoding completed...")
-        return embedding
+        embedding = model.encode(text, convert_to_tensor=True)
+        return embedding.tolist()
     except Exception as e:
         print(f"Error encoding text: {e}")
         raise
